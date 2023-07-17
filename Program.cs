8a62f441b4dcf0076356f688e8ca407b8b47a9fd
Denis-Moiseev <mrtesla666@gmail.com>
2023-07-17 23:45:32 +0800

    finished

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int GetInput(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int num = GetInput("Введите число: ");
+int variable = 1;
+while (variable <= num)
+{
+    double resalt = Math.Pow(variable,3);
+    variable++;
+    Console.Write($"{resalt} ");
+}
